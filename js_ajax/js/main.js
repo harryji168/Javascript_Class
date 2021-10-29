19749580e36ed25d7aa69db058b7804ae1110b7a
Harry Ji <jiharry@hotmail.com>
2021-10-29 10:42:13 -0700

    Fri 29 Oct 2021 10:42:13 AM PDT

--- js_ajax/js/main.js
+++ js_ajax/js/main.js
@@ -1,10 +1,37 @@
 const fetchButton = document.querySelector("#fetch-button");
 
 fetchButton.addEventListener('click', event => {
-    // 1. XMLHttpRequest
-    // to use this, we need to create an instance of it
-    const xhrStudents = new XMLHttpRequest();
+    if (false) {
+        //#region  1. XMLHttpRequest
+        // to use this, we need to create an instance of it
+        const xhrStudents = new XMLHttpRequest();
 
-    xhrStudents.open("GET", "http://localhost:3000/students")
+        xhrStudents.open("GET", "http://localhost:3000/students");
+        // open method accepts two arguments, 
+        // the first is to specify the http method => "GET", "POST", "DELETE", "PATCH"
+        // the second is the url
+
+        xhrStudents.onload = () => {
+            // what are we going to do once we got the response
+            console.log("Students: ", JSON.parse(xhrStudents.responseText));
+        }
+
+        xhrStudents.send();
+
+        const xhrDepartments = new XMLHttpRequest();
+        xhrDepartments.open("GET", "http://localhost:3000/departments");
+        xhrDepartments.onload = () => {
+            console.log("departments: ", JSON.parse(xhrDepartments.responseText));
+        }
+        xhrDepartments.send();
+        //#endregion
+    }
+
+    $.ajax({
+        url: "http://localhost:3000/students",
+        success: (responseData) => {
+            console.log("jQuery: ", responseData);
+        }
+    })
 
 })
